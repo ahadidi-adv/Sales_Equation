0f66c9db720946ff2cbc4935837113afbe4f5f0b
ahadidi-adv <167100561+ahadidi-adv@users.noreply.github.com>
2025-02-14 00:39:11 +0100

    Update index.py

--- Project/index.py
+++ Project/index.py
@@ -5,18 +5,19 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
-html_string='''
-<script>
-// To break out of iframe and access the parent window
-const streamlitDoc = window.parent.document;
+footer="
 
-// Make the replacement
-document.addEventListener("DOMContentLoaded", function(event){
-        streamlitDoc.getElementsByTagName("footer")[0].innerHTML = "Provided by <a href='https://yourwebsite.com' target='_blank' class='css-z3au9t egzxvld2'>Your Link Display Text Here</a>";
-    });
-</script>
-'''
-components.html(html_string)
+<style> your css code put here</style>
+
+<div class='footer'>
+
+<p>the word you want to tell<a style='display:block;text-align:center;' 
+
+href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
+
+</div>"
+
+st.markdown(footer, unsafe_allow_html=True)
 st.logo("Africa.png", icon_image="Logo.png")
 
 st.write("# Calibrage et optimisation des visites commerciales 🎯")
