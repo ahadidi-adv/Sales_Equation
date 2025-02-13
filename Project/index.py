df3dbb4214f89e97c18232c66eee75925eae2bd3
ahadidi-adv <167100561+ahadidi-adv@users.noreply.github.com>
2025-02-14 00:34:45 +0100

    Update index.py

--- Project/index.py
+++ Project/index.py
@@ -1,15 +1,16 @@
 import streamlit as st
-
-st.set_page_config(
-    page_title="Hello",
-    page_icon="👋",
-)
- <style>
+hide_streamlit_style = """
+            <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
 st.logo("Africa.png", icon_image="Logo.png")
 
 st.write("# Calibrage et optimisation des visites commerciales 🎯")
