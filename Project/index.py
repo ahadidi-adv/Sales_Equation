a5c1cf89542030063cc11e0e93fcb02ff9394be8
ahadidi-adv <167100561+ahadidi-adv@users.noreply.github.com>
2025-02-14 00:33:24 +0100

    Update index.py

--- Project/index.py
+++ Project/index.py
@@ -4,13 +4,18 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
-hide_streamlit_style = """
-            <style>
-            [data-testid="stToolbar"] {visibility: hidden !important;}
-            footer {visibility: hidden !important;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+html_string='''
+<script>
+// To break out of iframe and access the parent window
+const streamlitDoc = window.parent.document;
+
+// Make the replacement
+document.addEventListener("DOMContentLoaded", function(event){
+        streamlitDoc.getElementsByTagName("footer")[0].innerHTML = "Provided by <a href='https://yourwebsite.com' target='_blank' class='css-z3au9t egzxvld2'>Your Link Display Text Here</a>";
+    });
+</script>
+'''
+components.html(html_string)
 st.logo("Africa.png", icon_image="Logo.png")
 
 st.write("# Calibrage et optimisation des visites commerciales 🎯")
