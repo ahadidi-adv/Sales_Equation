dcaab775c2baa365d0be1b15d36fe945ce3a30fb
ahadidi-adv <167100561+ahadidi-adv@users.noreply.github.com>
2025-02-14 00:39:35 +0100

    Update index.py

--- Project/index.py
+++ Project/index.py
@@ -5,19 +5,20 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
-footer="
-
-<style> your css code put here</style>
-
-<div class='footer'>
-
-<p>the word you want to tell<a style='display:block;text-align:center;' 
-
-href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
-
-</div>"
-
-st.markdown(footer, unsafe_allow_html=True)
+footer {
+	
+	visibility: hidden;
+	
+	}
+footer:after {
+	content:'goodbye'; 
+	visibility: visible;
+	display: block;
+	position: relative;
+	#background-color: red;
+	padding: 5px;
+	top: 2px;
+}
 st.logo("Africa.png", icon_image="Logo.png")
 
 st.write("# Calibrage et optimisation des visites commerciales 🎯")
