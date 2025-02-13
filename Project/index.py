daf9118944e2f56b07bedfe7e4bbebd8f7603859
ahadidi-adv <167100561+ahadidi-adv@users.noreply.github.com>
2025-02-14 00:31:40 +0100

    Update index.py

--- Project/index.py
+++ Project/index.py
@@ -4,7 +4,13 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
-
+hide_streamlit_style = """
+            <style>
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 st.logo("Africa.png", icon_image="Logo.png")
 
 st.write("# Calibrage et optimisation des visites commerciales 🎯")
