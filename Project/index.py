8be6f4942bbd65039f69ece5f088a0885480e032
ahadidi-adv <167100561+ahadidi-adv@users.noreply.github.com>
2025-02-14 00:42:03 +0100

    Update index.py

--- Project/index.py
+++ Project/index.py
@@ -5,10 +5,10 @@ st.set_page_config(
     page_title="Hello",
     page_icon="👋",
 )
-# Hide "Hosted with Streamlit" footer
 hide_streamlit_style = """
             <style>
-            footer {visibility: hidden;}
+            [data-testid="stToolbar"] {visibility: hidden !important;}
+            footer {visibility: hidden !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
