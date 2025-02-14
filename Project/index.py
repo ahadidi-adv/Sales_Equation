feb2dc81dfa1a792d17f81d86bdded9e936b5edf
ahadidi-adv <167100561+ahadidi-adv@users.noreply.github.com>
2025-02-14 01:11:33 +0100

    Update index.py

--- Project/index.py
+++ Project/index.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit.components.v1 import html
-    html('''
+html('''
        <script>
         window.top.document.querySelectorAll(`[href*="streamlit.io"]`).forEach(e => e.setAttribute("style", "display: none;"));
       </script>
